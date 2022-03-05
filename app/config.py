938647eb7698525e63690e5876e348a8d60d4c95
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-03-05 17:36:15 -0500

    add sample .env file and updated config file

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ load_dotenv()  # load environment variables from .env if it exists.
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
