d18a4c94169794aaa41dc7af52e51d7520568ab0
Coder-Jon014 <jon.coombs14@gmail.com>
2022-03-16 11:20:43 -0500

    Edited Migration Folder

--- migrations/versions/20c0082bd53b_.py
+++ migrations/versions/20c0082bd53b_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '20c0082bd53b'
-down_revision = 'properties'
+down_revision = None
 branch_labels = None
 depends_on = None
 
